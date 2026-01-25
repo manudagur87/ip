4ac2f8e76d82b424846f97b2135d59e38a5a240b
manudagur87 <manudagur87@gmail.com>
2026-01-25 16:29:20 +0800

    Level-0

---
+++ src/main/java/Friday.java
@@ -0,0 +1,11 @@
+public class Friday {
+    public static void main(String[] args) {
+        String logo ="______________________________\n" +
+                "|  ________________________  |\n" +
+                "| |                        | |\n" +
+                "| |       FRIDAY           | |\n" +
+                "| |________________________| |\n" +
+                "|____________________________|\n";
+        System.out.println("Hello I'm\n" + logo + "What can I do for you?");
+    }
+}
